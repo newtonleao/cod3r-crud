9f2bf160ee4cdc105b1db45351c5173e03be0c2d
NewtonLeao <newtonleao@hotmail.com>
2020-06-24 19:45:46 -0300

    Adição do form

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+
+<Main icon="home" title="Início" 
+    subtitle="Segundo projeto do capitulo do React">
+    <div className="display-4">Bem Vindo!</div>
+    <hr />
+    <p className="mb-0">Sistema exemplo de um cadastro desenvolvido em React</p>
+</Main>
--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo192.png'
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default props => 
 <aside className="logo">
-    logo
+    <Link to="/" className="logo">
+        <img src={logo} alt="logo" />
+    </Link>
 </aside>
